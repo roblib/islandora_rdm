8f308c6b4fec64bfb99e24f13595c784346c012c
Alexander O'Neill <aoneill@upei.ca>
2019-03-28 15:32:32 -0300

    Fix null identifier error in Datacite export.

--- islandora_rdm_datacite/src/Dataset.php
+++ islandora_rdm_datacite/src/Dataset.php
@@ -16,14 +16,16 @@ class Dataset {
       ksm($node->getType());
       return FALSE;
     }
-    $xsins = 'http://www.w3.org/2001/XMLSchema-instance';
+
     $this->doc = \DOMDocument::loadXml('<resource xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://datacite.org/schema/kernel-4" xsi:schemaLocation="http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4.1/metadata.xsd"></resource>');
 
     // Identifier
     $resource = $this->doc->documentElement;
-    $identifier = $this->doc->createElement('identifier', $node->get('field_islandora_rdm_identifier')->first()->getString());
-    $identifier->setAttribute('identifierType', 'DOI');
-    $resource->appendChild($identifier);
+    if ($identifier_field = $node->get('field_islandora_rdm_identifier')->first()) {
+      $identifier = $this->doc->createElement('identifier', $identifier_field->getString());
+      $identifier->setAttribute('identifierType', 'DOI');
+      $resource->appendChild($identifier);
+    }
 
     // Creator
     $creators_element = $this->doc->createElement('creators');
