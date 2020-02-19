34e99812eaa0b46501b06cecc75d32de65ebfa6b
Alan Stanley <astanley@upei.ca>
2020-02-19 14:27:07 -0400

    File transmission Fixity

---
+++ islandora_rdm_file_transmission_fixity/src/Form/FixitySettingForm.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\islandora_rdm_file_transmission_fixity\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure example settings for this site.
+ */
+class FixitySettingForm extends ConfigFormBase {
+  /**
+   * The path to stored config file.
+   *
+   * @var string
+   */
+  protected $config_filepath;
+
+  public function getFormId() {
+    return 'islandora_rdm_file_transmission_fixity_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'islandora_rdm_file_transmission_fixity.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('islandora_rdm_file_transmission_fixity.settings');
+
+    $form['expected_fixity_field'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Machine name of expected checksum field'),
+      '#description' => $this->t('Each Media Type must have a field allowing the user to enter the SHA1 checksum of the file to be uploaded.'),
+      '#default_value' => $config->get('expected_fixity_field') ? $config->get('expected_fixity_field') : 'field_expected_checksum',
+    ];
+    $form['actual_fixity_field'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Machine name of actual checksum field'),
+      '#description' => $this->t('Each Media Type must have a field allowing the user to enter the SHA1 checksum of the file to be uploaded.'),
+      '#default_value' => $config->get('actual_fixity_field') ? $config->get('actual_fixity_field') : 'field_actual_checksum',
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('islandora_rdm_file_transmission_fixity.settings')
+      ->set('expected_fixity_field', trim($form_state->getValue('expected_fixity_field')))
+      ->set('actual_fixity_field', trim($form_state->getValue('actual_fixity_field')))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+}
+
