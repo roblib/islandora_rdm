58df8637e34ffd4de16e240e1cba19c93f4fc7c4
ajstanley <alanjarlathstanley@gmail.com>
2020-04-13 10:28:39 -0300

    new config files for geolocation

--- islandora_rdm_datacite/src/Dataset.php
+++ islandora_rdm_datacite/src/Dataset.php
@@ -40,7 +40,8 @@ class Dataset {
     // Identifier.
     $resource = $this->doc->documentElement;
 
-    if ($identifier_field = $node->get('field_islandora_rdm_identifier')->first()) {
+    if ($identifier_field = $node->get('field_islandora_rdm_identifier')
+      ->first()) {
       $identifier = $this->doc->createElement('identifier', $identifier_field->getString());
       $identifier->setAttribute('identifierType', 'DOI');
       $resource->appendChild($identifier);
@@ -53,7 +54,8 @@ class Dataset {
 
     if ($resource_type_field = $node->get('field_rdm_resource_type')->first()) {
       $resource_type = $this->doc->createElement('resourceType', $resource_type_field->getString());
-      if ($resource_type_general_field = $node->get('field_rdm_resource_type_general')->first()) {
+      if ($resource_type_general_field = $node->get('field_rdm_resource_type_general')
+        ->first()) {
         $resource_type->setAttribute('resourceTypeGeneral', $resource_type_general_field->getString());
       }
       $resource->appendChild($resource_type);
@@ -99,8 +101,10 @@ class Dataset {
     if ($additional_date_fields = $node->get('field_date_paragraph')) {
       foreach ($additional_date_fields as $additional_date_field) {
         if ($date_target = $additional_date_field->get('entity')->getTarget()) {
-          $additional_date_string = $date_target->get('field_date_string')->getString();
-          $additional_date_type = $date_target->get('field_type_of_date')->getString();
+          $additional_date_string = $date_target->get('field_date_string')
+            ->getString();
+          $additional_date_type = $date_target->get('field_type_of_date')
+            ->getString();
           if (!empty($additional_date_type) && !empty($additional_date_string)) {
             $additional_date_element = $this->doc->createElement('date', $additional_date_string);
             $additional_date_element->setAttribute('dateType', $additional_date_type);
@@ -130,21 +134,55 @@ class Dataset {
     $description_count = 0;
     if ($descriptions_field = $node->get('field_rdm_description')) {
       foreach ($descriptions_field as $description_field) {
-        if ($description_target = $description_field->get('entity')->getTarget()) {
-          $description_string = $description_target->get('field_rdm_description')->getString();
-          $description_type = $description_target->get('field_rdm_description_type')->getString();
-          $description_element = $this->doc->createElement('description', $description_string);
-          $description_element->setAttribute('descriptionType', $description_type);
-          $descriptions_element->appendChild($description_element);
-          $description_count += 1;
+        if ($description_target = $description_field->get('entity')
+          ->getTarget()) {
+          $description_string = $description_target->get('field_rdm_description')
+            ->getString();
+          $description_type = $description_target->get('field_rdm_description_type')
+            ->getString();
+          if ($description_string) {
+            $description_element = $this->doc->createElement('description', $description_string);
+            $description_element->setAttribute('descriptionType', $description_type);
+            $descriptions_element->appendChild($description_element);
+            $description_count += 1;
+          }
+
         }
       }
     }
-
     if ($description_count > 0) {
       $resource->appendChild($descriptions_element);
     }
+    // Spatial Properties
+    if ($location_data = $node->get('field_physical_location')) {
+      if ($location_data->count()) {
+        $geolocations_element = $this->doc->createElement('Geolocations');
+        foreach ($location_data as $location_datum) {
+          $geolocation_element = $this->doc->createElement('GeoLocation');
+          $geolocations_element->appendChild($geolocation_element);
 
+          if ($location_target = $location_datum->get('entity')->getTarget()) {
+            $location_string = $location_target->get('field_geolocation')
+              ->getString();
+            if ($location_string) {
+              $geolocationplace = $this->doc->createElement('geoLocationPlace', $location_string);
+              $geolocation_element->appendChild($geolocationplace);
+            }
+            $coordinates = $location_target->get('field_geolocationpoint_')
+              ->getValue();
+            if ($coordinates) {
+              $geolocationpoint = $this->doc->createElement('geoLocationPoint');
+              $geolocation_element->appendChild($geolocationpoint);
+              $long = $this->doc->createElement('pointLongitude', $coordinates[0]['lng']);
+              $lat = $this->doc->createElement('pointLatitude', $coordinates[0]['lat']);
+              $geolocationpoint->appendChild($long);
+              $geolocationpoint->appendChild($lat);
+            }
+          }
+        }
+        $resource->appendChild($geolocations_element);
+      }
+    }
     return $this->doc->saveXML();
   }
 
@@ -171,26 +209,42 @@ class Dataset {
         $researcher = $researcher_target->getValue();
         // Contributors have roles.
         if ($is_contributor = $researcher->getType() == 'rdm_contribution_personal') {
-          $contributor_type = $researcher->get('field_rdm_role_personal')->first()->getString();
+          $contributor_type = $researcher->get('field_rdm_role_personal')
+            ->first()
+            ->getString();
           $wrapper_element->setAttribute('contributorType', $contributor_type);
-          $researcher = $researcher->get('field_rdm_person')->first()->get('entity')->getTarget()->getValue();
+          $researcher = $researcher->get('field_rdm_person')
+            ->first()
+            ->get('entity')
+            ->getTarget()
+            ->getValue();
         }
         elseif ($is_contributor = $researcher->getType() == 'rdm_contribution_organizational') {
-          $contributor_type = $researcher->get('field_rdm_role_organizational')->first()->getString();
+          $contributor_type = $researcher->get('field_rdm_role_organizational')
+            ->first()
+            ->getString();
           $wrapper_element->setAttribute('contributorType', $contributor_type);
-          $researcher = $researcher->get('field_rdm_organization')->first()->get('entity')->getTarget()->getValue();
+          $researcher = $researcher->get('field_rdm_organization')
+            ->first()
+            ->get('entity')
+            ->getTarget()
+            ->getValue();
         }
         $is_person = $researcher->getType() == 'rdm_person';
 
         // Person.
         if ($is_person) {
-          $name = $researcher->get('field_rdm_personal_name')->first()->getString();
+          $name = $researcher->get('field_rdm_personal_name')
+            ->first()
+            ->getString();
           $name_element = $this->doc->createElement($element_name . 'Name', $name);
           $name_element->setAttribute('nameType', 'Personal');
         }
         // Organization.
         else {
-          $name = $researcher->get('field_rdm_organization_name')->first()->getString();
+          $name = $researcher->get('field_rdm_organization_name')
+            ->first()
+            ->getString();
           $name_element = $this->doc->createElement($element_name . 'Name', $name);
           $name_element->setAttribute('nameType', 'Organizational');
         }
@@ -201,8 +255,12 @@ class Dataset {
         foreach ($researcher->get('field_rdm_personal_identifier') as $person_id) {
           $person_id = $person_id->get('entity')->getTarget();
           if (!empty($person_id->get('field_rdm_identifier_string')->first()) &&
-              $id = $person_id->get('field_rdm_identifier_string')->first()->getString()) {
-            $scheme = $person_id->get('field_rdm_identifier_type_person')->first()->getString();
+            $id = $person_id->get('field_rdm_identifier_string')
+              ->first()
+              ->getString()) {
+            $scheme = $person_id->get('field_rdm_identifier_type_person')
+              ->first()
+              ->getString();
             $id_element = $this->doc->createElement('nameIdentifier', $id);
             $id_element->setAttribute('nameIdentifierScheme', $scheme);
             $wrapper_element->appendChild($id_element);
@@ -214,8 +272,12 @@ class Dataset {
         foreach ($researcher->get('field_rdm_organizational_ids') as $org_id) {
           $org_id = $org_id->get('entity')->getTarget();
           if (!empty($org_id->get('field_rdm_identifier_string')->first()) &&
-              $id = $org_id->get('field_rdm_identifier_string')->first()->getString()) {
-            $scheme = $org_id->get('field_rdm_identifier_type_org')->first()->getString();
+            $id = $org_id->get('field_rdm_identifier_string')
+              ->first()
+              ->getString()) {
+            $scheme = $org_id->get('field_rdm_identifier_type_org')
+              ->first()
+              ->getString();
             $id_element = $this->doc->createElement('nameIdentifier', $id);
             $id_element->setAttribute('nameIdentifierScheme', $scheme);
             $wrapper_element->appendChild($id_element);
@@ -223,9 +285,15 @@ class Dataset {
         }
       }
       // Affiliation (person)
-      if ($is_person && !empty($researcher->get('field_rdm_personal_affiliation')->first())) {
-        $affiliation = $researcher->get('field_rdm_personal_affiliation')->first()->get('entity')->getTarget();
-        $affiliation_name = $affiliation->get('field_rdm_organization_name')->first()->getString();
+      if ($is_person && !empty($researcher->get('field_rdm_personal_affiliation')
+          ->first())) {
+        $affiliation = $researcher->get('field_rdm_personal_affiliation')
+          ->first()
+          ->get('entity')
+          ->getTarget();
+        $affiliation_name = $affiliation->get('field_rdm_organization_name')
+          ->first()
+          ->getString();
         $affiliation_element = $this->doc->createElement('affiliation', $affiliation_name);
         $wrapper_element->appendChild($affiliation_element);
       }
